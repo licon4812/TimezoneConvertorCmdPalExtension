6d775c176515fa5f40bea2faefc71c0b9c032244
James Alickolli <jalickolli@gmail.com>
2025-04-14 13:27:03 +1000

    initial commit

---
+++ src/TimezoneConvertorCmdPalExtension/TimezoneConvertorCmdPalExtensionCommandsProvider.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace TimezoneConvertorCmdPalExtension;
+
+public partial class TimezoneConvertorCmdPalExtensionCommandsProvider : CommandProvider
+{
+    private readonly ICommandItem[] _commands;
+
+    public TimezoneConvertorCmdPalExtensionCommandsProvider()
+    {
+        DisplayName = "Time zone Convertor for Command Palette";
+        Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
+        _commands = [
+            new CommandItem(new TimezoneConvertorCmdPalExtensionPage()) { Title = DisplayName },
+        ];
+    }
+
+    public override ICommandItem[] TopLevelCommands()
+    {
+        return _commands;
+    }
+
+}
